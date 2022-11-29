c89b884c7f8371681d7aad29f4e9d0c47ad02d37
Leo Lai <scipio1516@gmail.com>
2022-11-29 09:25:28 -0600

    Initial Commit

---
+++ Bowling Score Counter/Bowling_Score_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bowling_Score_CounterApp.swift
+//  Bowling Score Counter
+//
+//  Created by Leo Lai on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Bowling_Score_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
