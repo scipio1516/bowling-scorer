e0d87cfb3b0b1cb16a19d0872d4ebec83339c9ab
Leo Lai <scipio1516@gmail.com>
2022-12-06 10:10:05 -0600

    updated Score, added formatting etc.

--- Bowling Score Counter/Scorer.swift
+++ Bowling Score Counter/Scorer.swift
@@ -11,31 +11,103 @@ struct Scorer: View {
     struct OneFrame: Identifiable {
         
         let id = UUID()
-        var firstRoll: Int
-        var secondRoll: Int
-        var fillRoll: Int
+        var firstScore: Int
+        var secondScore: Int
+        var fillScore: Int
         var isSpare: Bool
         var isStrike: Bool
-        var isLastFrame: Bool
     }
-    private var scores: [OneFrame] = []
+    /*
+    struct frameView<Object: View {
+        let frame:
+        var body: some View {
+            VStack { Text("\(frame.firstScore)").font(.custom("AvenirNext-Bold", size: 18.0))
+                    .preferredColorScheme(.dark)
+                    .foregroundColor(.yellow)
+                Text("\(frame.secondScore)").font(.custom("AvenirNext-Bold", size: 18.0))
+                    .preferredColorScheme(.dark)
+                    .foregroundColor(.yellow)
+            }
+            .foregroundColor(.gray)
+            .padding(4)
+            .border(.purple)
+        }
+    }
+    */
+    @State private var scores: [OneFrame] = [OneFrame](repeating: OneFrame(firstScore: 0, secondScore: 0, fillScore: 0, isSpare: false, isStrike: false), count: 10)
+    @State private var buttons = [0,1,2,3,4,5,6,7,8,9,10]
+    @State private var currentFrame = 0
+    @State private var currentTotal = 0
+    @State private var currentRoll = 1
     var body: some View {
-        
-        HStack {
-            ForEach(scores) {
-                score in Text("\(score.firstRoll) / \(score.secondRoll)").font(.custom("AvenirNext-Bold", size: 13.0))
+    
+        VStack {
+            Button("3") {
+                updateScore(number: 3)
+            }.padding(6.969) // hi
+                .border(.white)
+            
+                .font(.custom("AvenirNext-Bold", size: 20.0))
+                .foregroundColor(Color(red:0.99, green: 0.1, blue:0.13))
+                .background(Color(red:0.60, green: 0.60, blue:0.93))
+            Text("Total: \(currentTotal)")
+                .font(.custom("AvenirNext-Bold", size: 25.0))
+            HStack {
+                //if I have time, make the 10 the same size as all the other ones
+                //after the first roll, show less buttons for each pin that was knocked down
+                ForEach(buttons, id: \.self) {button in if(button <= 10 - scores[currentFrame].firstScore) {
+                    Button("\(button)") {
+                        updateScore(number: button)
+                    }.padding(6.969) // hi
+                        .border(.white)
+                    
+                        .font(.custom("AvenirNext-Bold", size: 20.0))
+                        .foregroundColor(Color(red:0.99, green: 0.1, blue:0.13))
+                        .background(Color(red:0.60, green: 0.60, blue:0.93))
+                }
+                }
+            }
+            HStack {
+                ForEach(scores) {
+                    score in
+                }
+            }
+            if(currentFrame == 9) {
+                Text("FillRoll: \(scores[9].fillScore)")
             }
         }
     }
-    init() {
-        for i in 1...10 {
-            scores.append(OneFrame(firstRoll: 0, secondRoll: 0, fillRoll: 0, isSpare: false, isStrike: false, isLastFrame: false))
-            if(i == 10) {
-                scores[i - 1].isLastFrame = true
+    
+    func updateScore(number: Int) {
+        currentTotal = calcTotal()
+            if(currentRoll == 1) {
+                scores[currentFrame].firstScore = number
+                currentRoll += 1
+            } else if(currentRoll == 2) {
+                scores[currentFrame].secondScore = number
+                if(currentFrame == 9) {
+                    currentRoll += 1
+                } else{
+                    currentRoll = 1
+                    currentFrame += 1
+                }
+                
+            } else if(currentRoll == 3) {
+                scores[currentFrame].fillScore = number
+                currentRoll += 1
             }
-        }
+        
+    }
+    
+    func calcTotal() ->Int {
+        var tempTotal = 0
+        
+        
+        return tempTotal
     }
 }
+
+
 
 struct Scorer_Previews: PreviewProvider {
     static var previews: some View {
